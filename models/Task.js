5b5352b4d1951904d4e15ad95dd9a094e9417572
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-05-04 19:10:23 -0500

    JWT Auth now works

--- models/Task.js
+++ models/Task.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('./connection');
+const { Schema, model } = require('../config/database');
 
 
 const taskSchema = Schema({
--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const taskController = require('./controllers/task');
 
+// Connect to the Database //
+require('./config/database.js');
+
 // Middleware //
 app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskController); // When you have /tasks, check the frontend if some of your routes are not working. Try /tasks/task
 
+// Check if token and create req.user
+app.use(require('./config/checkToken'));
+
+// API //
+app.use('/api/users', require('./routes/api/users'));
 
 // Listening Route //
 app.listen(PORT, () => {
